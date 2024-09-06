3409c43e8554bc61d3c0a2e07678fe919fe0446b
vidhan <20010418vidhan@gmail.com>
2024-09-06 19:16:01 +0530

    First Commit of the day , remember ray

---
+++ DjangoAPI/EmployeeAPP/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Departments(models.Model):
+    DepartmentID = models.AutoField(primary_key=True)
+    DepartmentName = models.CharField(max_length=100)
+
+class Employees(models.Model):
+    EmployeeID= models.AutoField(primary_key=True)
+    EmployeeName=models.CharField(max_length=100)
+    Department=models.CharField(max_length=100)
+    DateofJoining=models.DateField()
+    PhotofileName=models.CharField(max_length=100)
+
